8078efa9da1d7f0cdfd133b291393979d61e6cc0
theristoweats <theristoweats@gmail.com>
2022-03-25 05:31:10 +0100

    daawwee23

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use("/eats/products", express.static("./eats/products"));
 //     stream.pipe(res);
 // })
 
-app.post('/eats/products/upload', upload.single('image'), async (req, res) => {
+app.post('/eats/products/upload', (req, res) => {
     if (req.files === null) {
       return res.status(400).json({ msg: 'No file uploaded' });
     }
